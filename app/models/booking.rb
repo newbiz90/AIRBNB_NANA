bc94968a0ea7b18048c1aaf7ae02590ad676c1fe
Zong Han Goh <zonghangoh@gmail.com>
2024-03-09 18:01:01 +0800

    add statuses to booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,9 @@
 class Booking < ApplicationRecord
+  STATUSES = %w[pending confirmed declined].freeze
+
   belongs_to :user
   belongs_to :listing
   has_one :review, dependent: :restrict_with_error
+
+  validates :status, inclusion: { in: STATUSES }
 end
