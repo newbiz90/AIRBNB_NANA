c64effcb034d0a63e82fb35a85b274a330bd7c78
ericsson chan <ericssonchann@gmail.com>
2024-03-09 14:46:32 +0800

    add username to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :photo
+
+  validates :username, presence: true, uniqueness: true
+
 end
