238f5dc99aea25fa1b127fbcb1845c5a4759bb86
Matthew Oh <newbiz90@hotmail.com>
2024-03-09 14:22:14 +0800

    added dp_user_photo sucessfully

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.1].define(version: 2024_03_09_034817) do
-=======
 ActiveRecord::Schema[7.1].define(version: 2024_03_09_053643) do
->>>>>>> 63d83a0 (Added username to user model)
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
