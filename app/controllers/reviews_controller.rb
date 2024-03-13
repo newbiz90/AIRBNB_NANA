9873c701894da773a4e0100dd3c0a55c7aef6334
ericsson chan <ericssonchann@gmail.com>
2024-03-13 19:58:00 +0800

    add reviews file

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,4 @@
 class ReviewsController < ApplicationController
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> d99a6ce (add files)
-<<<<<<< HEAD
-=======
-
->>>>>>> 0623650 (add reviews files)
-<<<<<<< HEAD
-=======
->>>>>>> 3ec17c2 (added reviews files)
-=======
->>>>>>> 23db50b48f0bc4780fd8088a3779dd19fc8bbcf7
->>>>>>> d99a6ce (add files)
   def create
     @review = Review.new(review_params)
     @review.booking = Booking.find(params[:booking_id])
