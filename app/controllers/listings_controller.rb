5003f5200cf43d5d1843eadc1c6c2aa0d35d2255
KY KWAN <embers3101@gmail.com>
2024-03-16 15:48:56 +0800

    listing page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,30 @@
 class ListingsController < ApplicationController
+
   def show
-    @listing = Listing.find(params[:id])
-    @booking = Booking.new
+    @listings = Listing.all
+    #@booking = Booking.new
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      redirect_to listings_path, notice: 'Listing was successfully created.' # Redirect to the index action
+    else
+      render :new
+    end
+  end
+
+  def index
+    @listings = Listing.includes(:user).all
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:title, :description, :price)
   end
 end
